320cb18027c5189152e20eb7bb4fd5b83fa330b9
Eduardo Sanches Bocato <dubocato@gmail.com>
2017-02-25 18:52:57 -0300

    Initial Commit

---
+++ ContextualErrorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContextualErrorView
+//
+//  Created by Eduardo Sanches Bocato on 25/02/17.
+//  Copyright © 2017 bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContextualErrorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContextualErrorView
+//
+//  Created by Eduardo Sanches Bocato on 25/02/17.
+//  Copyright © 2017 bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
